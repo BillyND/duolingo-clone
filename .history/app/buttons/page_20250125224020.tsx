74f3dbedf6a95074cde5a7bac74a960b7f8c8727
Billy <longnd1@bravebits.vn>
2025-01-25 22:41:05 +0700

    Update folder structure

---
+++ .history/app/buttons/page_20250125224020.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ButtonPage(props) {
+  return <div>Button Page</div>;
+}
+
+export default ButtonPage;
---
+++ .history/app/buttons/page_20250125224023.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ButtonPage() {
+  return <div>Button Page</div>;
+}
+
+export default ButtonPage;

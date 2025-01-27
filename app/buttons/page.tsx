9360f4bdfeaf6fba1038c98c2f79800f760ef0c2
Billy <longnd1@bravebits.vn>
2025-01-27 20:47:24 +0700

    Add Button Component

--- app/buttons/page.tsx
+++ app/buttons/page.tsx
@@ -1,9 +1,11 @@
+import { Button } from "@/components/ui/button";
 import React from "react";
 
 function ButtonPages() {
   return (
-    <div>
-      <button>hello</button>
+    <div className="p-4 flex flex-col gap-4">
+      <Button>Primary</Button>
+      <Button>Primary Outline</Button>
     </div>
   );
 }

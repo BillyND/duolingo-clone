139849975ebb8b7bc735cf56aac9d7d4c0827251
Billy <longnd1@bravebits.vn>
2025-01-29 19:07:17 +0700

    Add marketing page

---
+++ app/(marketing)/layout.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import React from "react";
+import Header from "./header";
+import Footer from "./footer";
+import { useIsMobile } from "@/hooks/use-mobile";
+
+function MarketingLayout({ children }: { children: React.ReactNode }) {
+  const isMobile = useIsMobile();
+
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Header />
+      <main className="flex-1 flex justify-center items-center">
+        {children}
+      </main>
+      {!isMobile && <Footer />}
+    </div>
+  );
+}
+
+export default MarketingLayout;

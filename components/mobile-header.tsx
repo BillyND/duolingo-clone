4df0ecc83c3d08fc887b48004bf6cbf7ee0cf0ec
Billy <longnd1@bravebits.vn>
2025-02-09 23:11:05 +0700

    Add mobile header

---
+++ components/mobile-header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MobileHeader = () => {
+  return (
+    <div className="lg:hidden h-[50px] px-6 fixed top-0 flex items-center justify-between bg-yellow-500 w-full z-50">
+      Mobile Header
+    </div>
+  );
+};
+
+export default MobileHeader;

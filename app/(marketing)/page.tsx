0e2f73ad05340da99424f0a8f5cfdf3605fc131b
Billy <longnd1@bravebits.vn>
2025-02-17 15:44:55 +0700

    Add button Get started for sign in

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import { useIsMobile } from "@/hooks/use-mobile";
-import { SignedOut } from "@clerk/clerk-react";
+import { SignedIn, SignedOut } from "@clerk/clerk-react";
 import { SignInButton } from "@clerk/nextjs";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
@@ -12,9 +12,8 @@ export default function MarketingPage() {
 
   return (
     <div
-      className={`p-4 w-full max-w-screen-lg items-center ${
-        isMobile ? "flex flex-col" : "flex flex-row mx-auto"
-      }`}
+      className={`p-4 w-full max-w-screen-lg items-center ${isMobile ? "flex flex-col" : "flex flex-row mx-auto"
+        }`}
     >
       <div className="flex flex-1 w-full justify-center">
         <Image
@@ -31,9 +30,20 @@ export default function MarketingPage() {
           Free, fun, and effective way to learn a language!
         </h1>
         <div className="pt-5 flex flex-col items-center space-y-2 gap-2">
-          <Button variant={"secondary"} onClick={() => router.push("/learn")}>
-            Get Started
-          </Button>
+          <SignedIn>
+            <Button variant={"secondary"} onClick={() => router.push("/learn")}>
+              Get Started
+            </Button>
+          </SignedIn>
+
+          <SignedOut>
+            <SignInButton mode="modal" forceRedirectUrl={"/learn"}>
+              <Button variant={"secondary"}  >
+                Get Started
+              </Button>
+            </SignInButton>
+          </SignedOut>
+
           <SignedOut>
             <SignInButton mode="modal" forceRedirectUrl={"/learn"}>
               <Button variant={"secondaryOutline"}>
